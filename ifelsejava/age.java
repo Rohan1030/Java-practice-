c636ea962d8ae08332062018168da5d665a464b3
Rohan Kumar Sharma <127482666+Rohan1030@users.noreply.github.com>
2024-11-02 20:28:49 +0530

    Add files via upload

---
+++ ifelsejava/age.java
@@ -0,0 +1,16 @@
+public class age {
+    public static void main(String[] args) {
+        int age = 66;
+        if(age >=20 && age<=30){
+            System.out.println("you are in your 20s");
+        }
+        else if(age>=30 && age <=40){
+            System.out.println("your are in your 40s");
+        }
+        else{
+            System.out.println("you are still a kid");
+        }
+
+    }
+    
+}
---
+++ ifelsejava/ageproblem.java
@@ -0,0 +1,14 @@
+public class ageproblem {
+    public static void main(String[] args) {
+        int age = 77;
+        if(age>=18 && age<=60){
+            System.out.println("you are an adult");
+        } 
+         else if(age>=60){
+            System.out.println("you are old");
+         }
+
+
+        }
+    }
+    

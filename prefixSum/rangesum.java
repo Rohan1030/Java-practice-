00d024fbdcd51bde3c67d84127e0e9815585e621
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-07-25 16:12:55 +0530

    learning about prifixsum

---
+++ prefixSum/rangesum.java
@@ -0,0 +1,36 @@
+package prefixSum;
+import java.util.*;
+
+
+public class rangesum {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // size of array
+    System.out.println("Enter the size of array: ");
+    int n = sc.nextInt();
+    // array with size n
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+      arr[i]=sc.nextInt();
+    }
+
+    System.out.println("Enter start : ");
+    int i = sc.nextInt();
+
+    System.out.println("Enter end : ");
+    int j = sc.nextInt();
+
+    int sum =0;
+    for(int k=i;k<=j;k++)
+    {
+     sum += arr[k];
+    }
+    System.out.println("Sum from index " + i + " to " + j + " is: " + sum);
+
+    
+
+  }
+
+  
+}

c6660da9601009b43a8328231c312b6ff0a6d25d
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-07-25 10:31:36 +0530

    updated

--- PrintingNumbers.java
+++
@@ -1,8 +0,0 @@
-public class PrintingNumbers {
-    public static void main(String[] args) {
-        System.out.println(7+11 );
-        
-    }
-
-    
-}
--- volumeofsphere.java
+++
@@ -1,9 +0,0 @@
-public class volumeofsphere {
-    public static void main(String[] args) {
-        double r = 6;
-        double pi = 3.14;
-        double volume = (4*pi*r*r*r)/3;
-        System.out.println(volume);
-    }
-    
-}
